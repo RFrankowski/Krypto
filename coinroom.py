ff03c318999532abfe373672c17866668969628d
RFrankowski <r.franek93@gmail.com>
2018-03-22 20:07:36 +0100

    update

--- coinroom.py
+++ coinroom.py
@@ -7,10 +7,10 @@ import  requests
 # r = requests.get('https://www.kraken.com/help/fees')
 r = requests.get('https://coinroom.com/tabela-oplat')
 soup = bs.BeautifulSoup(r.text, 'lxml')
-for tabele in soup.find_all('table', 'payment-table'):
-    # print tabele.text
-    for row in tabele.tr:
-        print row
+tabela = soup.find_all('table', 'payment-table')
+for tr in tabela.find_all('tbody'):
+    print tr
+
 
 
 
