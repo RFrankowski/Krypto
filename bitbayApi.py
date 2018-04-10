93a65d4d047df456135e8cf98136dce644a42374
RFrankowski <r.franek93@gmail.com>
2018-04-10 18:51:49 +0200

    testowanie api

--- bitbayApi.py
+++ bitbayApi.py
@@ -5,10 +5,20 @@ import bs4 as bs
 import lxml
 import re
 import requests
+import json
+
+r = requests.get("https://bitbay.net/API/Public/BTCPLN/orderbook.json")
+
+print json.dumps( r.json())
+
+bids = json.dumps(r.json()['bids'])
+print bids
 
 
-r = requests.get("https://bitbay.net/API/Public/BTCPLN/ticker.json")
 
-print r.json()
+# asks = json.dumps(r.json()['asks'])
+# print asks
 
+# transactions = json.dumps(r.json()['transactions'])
+# print transactions
 
