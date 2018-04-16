15fc7cea90bd11138d767ec9d094db9aee522994
RFrankowski <r.franek93@gmail.com>
2018-04-16 20:11:19 +0200

    praca nad api

--- bitbay.py
+++ bitbay.py
@@ -4,29 +4,31 @@ import lxml
 import re
 from exceptions import Exception
 
-sock = urllib.urlopen("https://bitbay.net/en/fees")
-htmlSource = sock.read()
-sock.close()
-soup = bs.BeautifulSoup(htmlSource, 'lxml')
 
-x = []
-# xx ={}
+def get_bitbay_withdrawals():
+
+    sock = urllib.urlopen("https://bitbay.net/en/fees")
+    htmlSource = sock.read()
+    sock.close()
+    soup = bs.BeautifulSoup(htmlSource, 'lxml')
+    list_kursow = []
+    try:
+        for item in soup.find_all('section', id='withdrawals'):
+            for listItem in item.find_all('li'):
+                if listItem.string is not None:
+                    waluta_cena = re.search(r'([aA-zZ]{3,4}:\s[0-9](\.*[0-9]{0,8}))', str(listItem.text)).string
+                    waluta, cena = waluta_cena.split(':')
+                    # xx[waluta] = cena
+                    list_kursow.append([str(waluta), float(cena)])
+    except Exception as e:
+        print e.args
+        print e.message
+        print "blad w parsowaniu: https://bitbay.net/en/fees"
+
+    return list_kursow
 
 
-list_kursow = []
-try:
-    for item in soup.find_all('section', id='withdrawals'):
-        for listItem in item.find_all('li'):
-            if listItem.string is not None:
-                waluta_cena = re.search(r'([aA-zZ]{3,4}:\s[0-9](\.*[0-9]{0,8}))', str(listItem.text)).string
-                waluta, cena = waluta_cena.split(':')
-                # xx[waluta] = cena
-                list_kursow.append([str(waluta), float(cena)])
-except Exception as e:
-    print e.args
-    print e.message
-    print "blad w parsowaniu: https://bitbay.net/en/fees"
+# print get_bitbay_withdrawals()
 
-print list_kursow
 
 
--- bitbayApiPubliczne.py
+++ bitbayApiPubliczne.py
@@ -1,15 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import requests
-crypto = "BTCPLN"
+import time
+from bitbay import get_bitbay_withdrawals
+
+# waluty dostepne na bit bay: pozniez zamienic na zaczyt z api/parsowanie
+waluty_bitbay = ['LTC', 'ETH']
+
+
 # get info from public api
-response = requests.get("https://bitbay.net/API/Public/"+crypto+"/orderbook.json")
-data = response.json()
-print(response)
-bid = []
-ask = []
-bids = data['bids']
 
-for item in bids:
-    bid.append(item)
-    print(item)
 
-print('pierwsza: ', bid[0])
+
+def znajdz_koszt_wycofania(waluta, koszt_wycofania):
+    for waluta_koszt in koszt_wycofania:
+        if waluta_koszt[0] == waluta:
+            return waluta_koszt[1]
+
+
+def wyswietl_koszt_wycofania_w_pln():
+    koszt_wycofania_bitbay = get_bitbay_withdrawals()
+    for waluta in waluty_bitbay:
+        response = requests.get("https://bitbay.net/API/Public/" + waluta + "PLN" + "/orderbook.json")
+
+        data = response.json()
+        # print data
+        bids = data['bids']
+        bid = []
+        for item in bids:
+            bid.append(item)
+        print('pierwsza: ', waluta, str(bid[0][0]) + " PLN", str(bid[0][1]) + " volum",
+              znajdz_koszt_wycofania(waluta, koszt_wycofania_bitbay) * bid[0][0])
+        time.sleep(1)
+
+def wymina_na_inna_walute():
+    koszt_wycofania_bitbay = get_bitbay_withdrawals()
+    for waluta in waluty_bitbay:
+        response = requests.get("https://bitbay.net/API/Public/" + waluta + "BTC" + "/orderbook.json")
+        data = response.json()
+        # print data
+        bids = data['bids']
+        bid = []
+        for item in bids:
+            bid.append(item)
+        print('pierwsza: ', waluta, str(bid[0][0]) + " btc", str(bid[0][1]) + " volum")
+        time.sleep(1)
+
+
+wymina_na_inna_walute()
+
+
+
