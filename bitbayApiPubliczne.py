5dc582bb8bf155a8e1ed5fca4631c4b522f5058b
RFrankowski <r.franek93@gmail.com>
2018-04-17 11:50:05 +0200

    zmiana struktury kodu

--- bitbayApiPubliczne.py
+++ bitbayApiPubliczne.py
@@ -4,43 +4,58 @@ import requests
 import time
 from bitbay import get_bitbay_withdrawals
 
-# waluty dostepne na bit bay: pozniez zamienic na zaczyt z api/parsowanie
-waluty_bitbay = ['BTC', 'LTC', 'ETH']
+
+# ================ bids - oferty kupna asks - oferty sprzedazy ===========================
+# ================ "maker" i "taker"======================================================
+# jak wystawiasz oferte to placisz 0.30% a jak bierzesz jakas oferte to cos kolo 0.40%
+# ================ wystawiasz czyli maker, bierzesz - taker ==============================
+
 
 
 # zwraca koszt wycofania dla danej waluty dla danych pobranych z giedly
-def znajdz_koszt_wycofania(waluta, koszt_wycofania):
+def calculate_withdrawals(waluta, koszt_wycofania, cena_sprzedazy):
     for waluta_koszt in koszt_wycofania:
         if waluta_koszt[0] == waluta:
-            return waluta_koszt[1]
+            return waluta_koszt[1] * cena_sprzedazy
 
 
 # funkcja zwraca koszt wycofania w walucie2
-def zwroc_koszt_wycofania(waluta, waluta2, koszt_wycofania_z_bitbay):
+def get_orderbook_first_offer(waluta, waluta2, kupno_sprzedaz):
     time.sleep(1)
     response = requests.get("https://bitbay.net/API/Public/" + waluta + waluta2 + "/orderbook.json")
     data = response.json()
     # print data
-    bids = data['bids']
-    bid = []
-    for item in bids:
-        bid.append(item)
+    asks = data[kupno_sprzedaz]
+    # ask = []
+    # for item in asks:
+    #     ask.append(item)
     # cena kupna jednej jednoski za walute2
-    cena_kupna = bid[0][0]
-    ilosc_w_ofercie = bid[0][1]
-
-    # return ('pierwsza najtansza oferta: ', waluta, str(cena_kupna) + " " + waluta2, str(ilosc_w_ofercie) + " volume",
-    #         znajdz_koszt_wycofania(waluta, koszt_wycofania_z_bitbay) * cena_kupna + "koszt wycofania z gieldy")
-    return znajdz_koszt_wycofania(waluta, koszt_wycofania_z_bitbay) * cena_kupna
+    # cena_kupna = ask[0][0]
+    # ilosc_w_ofercie = ask[0][1]
+    # zwraca pierwsza oferte sprzedazy
+    return asks[0]
 
 
 def main():
+    # pozniej dodac zaczyt z bitbay
+    waluty_bitbay = ['LSK', 'LTC', 'ETH']
+
     koszt_wycofania_bitbay = get_bitbay_withdrawals()
-    # sprawdzenie kosztu wycofrania w zlotowkach
-    # dodac wszystko do listy lub slownika
+
+    # sprawdzenie kosztu wycofrania w zlotowkach dla wszystkich walut
     for waluta in waluty_bitbay:
-        print waluta + " " + str(zwroc_koszt_wycofania(waluta, "PLN", koszt_wycofania_bitbay)) + "PLN"
-    # sprawdzic opcje przewalutowania i wycofania z danej gieldy
+        cena_ilosc = get_orderbook_first_offer(waluta, "PLN", "bids")
+        cena = cena_ilosc[0]
+        print waluta + " " + str(calculate_withdrawals(waluta, koszt_wycofania_bitbay, cena)) + "pln"
+
+    # przewalutowanie
+    for waluta in waluty_bitbay:
+        cena_ilosc = get_orderbook_first_offer(waluta, "BTC", "asks")
+        cena = cena_ilosc[0]
+        print cena_ilosc
+
+
+        # sprawdzic opcje przewalutowania i wycofania z danej gieldy
         # np btc > game sprawdzic koszt (przewalutowania + wycofania)
         # czynnosc powtorzyc dla wszystkich par
 
