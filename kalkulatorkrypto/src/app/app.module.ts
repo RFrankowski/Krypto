7abfb0c99a17dfb7bcbc0d973d27e0cdc0923f76
oskacz <oskiceins@gmail.com>
2018-05-24 10:46:05 +0200

    stworzone komponenty

--- kalkulatorkrypto/src/app/app.module.ts
+++ kalkulatorkrypto/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { KalkulatorComponent } from './body/kalkulator/kalkulator.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    KalkulatorComponent
   ],
   imports: [
     BrowserModule
